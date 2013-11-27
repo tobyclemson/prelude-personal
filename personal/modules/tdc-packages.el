8d09ac524b8ceebfd574e559473be5e89f5573ef
Toby Clemson <tobyclemson@gmail.com>
2013-11-26 23:31:51 -0200

    Adding keybindings in tdc-packages in emacs customisation.

--- personal/modules/tdc-packages.el
+++ personal/modules/tdc-packages.el
@@ -1,3 +1,11 @@
 (prelude-ensure-module-deps '(multiple-cursors))
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-M-=") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (provide 'tdc-packages)
